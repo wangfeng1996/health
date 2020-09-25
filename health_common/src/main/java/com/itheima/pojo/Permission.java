9e62e786201c8cc5c726432fa5d80144ba7a845f
“wangfeng1996” <1290715192@qq.com>
2020-09-25 11:08:44 +0800

    权限 实体类

--- health_common/src/main/java/com/itheima/pojo/Permission.java
+++ health_common/src/main/java/com/itheima/pojo/Permission.java
@@ -1,5 +1,9 @@
 package com.itheima.pojo;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.io.Serializable;
 import java.util.HashSet;
 import java.util.Set;
@@ -7,6 +11,8 @@ import java.util.Set;
 /**
  * 权限
  */
+@Data
+
 public class Permission implements Serializable{
     private Integer id;
     private String name; // 权限名称
@@ -14,43 +20,4 @@ public class Permission implements Serializable{
     private String description; // 描述
     private Set<Role> roles = new HashSet<Role>(0);
 
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getKeyword() {
-        return keyword;
-    }
-
-    public void setKeyword(String keyword) {
-        this.keyword = keyword;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public Set<Role> getRoles() {
-        return roles;
-    }
-
-    public void setRoles(Set<Role> roles) {
-        this.roles = roles;
-    }
 }
