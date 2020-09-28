0ae8640a1eb9d7f179cb14a393baf8fdbc2bcc68
“wangfeng1996” <1290715192@qq.com>
2020-09-28 09:43:07 +0800

    检查项的dao接口

--- health_service_provider/src/main/java/com/itheima/dao/CheckItemDao.java
+++ health_service_provider/src/main/java/com/itheima/dao/CheckItemDao.java
@@ -2,6 +2,7 @@ package com.itheima.dao;
 
 import com.github.pagehelper.Page;
 import com.itheima.pojo.CheckItem;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
@@ -12,39 +13,42 @@ public interface CheckItemDao {
     /**
      * 添加
      *
-     * @param checkItem  参数是检查项对象
+     * @param checkItem 参数是检查项对象
      */
     public void add(CheckItem checkItem);
 
     /**
-     *
-     * @param queryString  查询的条件，就是搜索框中查询的条件
+     * @param queryString 查询的条件，就是搜索框中查询的条件
      * @return
      */
 
-    public Page<CheckItem> selectByCondition(String queryString);
+    public Page<CheckItem> selectByCondition(@Param("value") String queryString);
 
     /**
      * 根据id进行删除
+     *
      * @param id
      */
     public void delete(Integer id);
 
     /**
      * 查找id是否关联外键
+     *
      * @param
      * @return
      */
-    public  long findCountId(Integer id);
+    public long findCountId(Integer id);
 
     /**
      * 修改
+     *
      * @param checkItem
      */
     public void update(CheckItem checkItem);
 
     /**
      * 根据id查询数据，并且回显给前端
+     *
      * @param id
      * @return
      */
@@ -52,6 +56,7 @@ public interface CheckItemDao {
 
     /**
      * 查询所有的检查项
+     *
      * @return
      */
 
