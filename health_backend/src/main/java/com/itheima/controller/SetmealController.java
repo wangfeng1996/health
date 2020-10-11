5051e03357763c5726ffdd1af546afe571bcb699
“wangfeng1996” <1290715192@qq.com>
2020-10-11 09:39:46 +0800

    检查套餐接口

---
+++ health_backend/src/main/java/com/itheima/controller/SetmealController.java
@@ -0,0 +1,84 @@
+package com.itheima.controller;
+
+import com.alibaba.dubbo.config.annotation.Reference;
+import com.itheima.constant.MessageConstant;
+import com.itheima.constant.RedisConstant;
+import com.itheima.entity.PageResult;
+import com.itheima.entity.QueryPageBean;
+import com.itheima.entity.Result;
+import com.itheima.pojo.Setmeal;
+import com.itheima.service.SetmealService;
+import com.itheima.utils.QiniuUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import redis.clients.jedis.JedisPool;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/setmeal")
+public class SetmealController {
+    @Reference
+    private SetmealService setmealService;
+    @Autowired
+    private JedisPool jedisPool;
+
+    /**
+     * 分页查询
+     *
+     * @param queryPageBean
+     * @return
+     */
+    @RequestMapping("/findPage")
+    public PageResult findPage(@RequestBody QueryPageBean queryPageBean) {
+        return setmealService.pageQuery(queryPageBean);
+    }
+
+    /**
+     * 新增套餐
+     *
+     * @param setmeal
+     * @param checkgroupIds
+     * @return
+     */
+    @RequestMapping("/add")
+    public Result add(@RequestBody Setmeal setmeal, Integer[] checkgroupIds) {
+        try {
+            setmealService.add(setmeal, checkgroupIds);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new Result(false, MessageConstant.ADD_SETMEAL_FAIL);
+        }
+        return new Result(true, MessageConstant.ADD_SETMEAL_SUCCESS);
+    }
+
+    /**
+     * 文件上传功能
+     */
+
+    @RequestMapping("/upload")
+    public Result upload(@RequestParam("imgFile") MultipartFile imgFile) {
+        //原始文件名
+        String originalFilename = imgFile.getOriginalFilename();
+
+        int index = originalFilename.lastIndexOf(".");
+        String suffix = originalFilename.substring(index);
+        String fileName = UUID.randomUUID().toString() + suffix;
+        //将图片保存到七牛云
+        try {
+            QiniuUtils.upload2Qiniu(imgFile.getBytes(), fileName);
+            //文件上传成功后，需要将文件名称保存到redis中
+            jedisPool.getResource().sadd(RedisConstant.SETMEAL_PIC_RESOURCES, fileName);
+            return new Result(true, MessageConstant.PIC_UPLOAD_SUCCESS, fileName);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new Result(false, MessageConstant.PIC_UPLOAD_FAIL);
+        }
+    }
+
+}
+
