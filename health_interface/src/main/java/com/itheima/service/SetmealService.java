531928f19a7ae26f2b12ef820bb85039b628aaaf
“wangfeng1996” <1290715192@qq.com>
2020-10-11 09:45:07 +0800

    检查套餐的接口信息

---
+++ health_interface/src/main/java/com/itheima/service/SetmealService.java
@@ -0,0 +1,23 @@
+package com.itheima.service;
+
+import com.itheima.entity.PageResult;
+import com.itheima.entity.QueryPageBean;
+import com.itheima.pojo.Setmeal;
+
+import java.util.List;
+
+/**
+ * @author wangfeng
+ */
+public interface SetmealService {
+
+    public void add(Setmeal setmeal, Integer[] checkgroupIds);
+
+    public PageResult pageQuery(QueryPageBean queryPageBean);
+
+    public List<Setmeal> findAll();
+
+    public Setmeal findById(int id);
+
+    public Setmeal findId(int id);
+}
